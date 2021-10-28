98c4d82b8b3cb250b85a06ff13e5f633ca93df63
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-28 12:57:26 +0500

    Add stemming

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 example_text = "Hello, how are you? The weather is great and Python is awesome."
 
@@ -18,3 +19,16 @@ words = word_tokenize(example_sentence)
 filtered_sentence = [w for w in words if not w in stop_words]
 
 print(filtered_sentence)
+
+ps = PorterStemmer()
+example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
+for w in example_words:
+    print(ps.stem(w))
+example_words = ["club","clubs","clubbing","clubed"]
+for w in example_words:
+    print(ps.stem(w))
+
+new_sentence = "It is very important to be pythonly while you are pythoning with python. All pythoners have pythoned poorly atleast once."
+words = word_tokenize(new_sentence)
+for w in words:
+    print(ps.stem(w))
