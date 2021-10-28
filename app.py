350a03f0350541d41e672839f98f99d38d12dc4e
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-28 12:29:02 +0500

    Tokenize by word

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = "Hello, how are you? The weather is great and Python is awesome."
+
+print(sent_tokenize(example_text))
+print(word_tokenize(example_text))
+
+for i in word_tokenize(example_text):
+    print(i)
