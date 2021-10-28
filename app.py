9a9c186f795eb8aae5e74e7fd5b82f3b5a53808d
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-28 12:46:05 +0500

    Add stop_words filter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 
 example_text = "Hello, how are you? The weather is great and Python is awesome."
 
@@ -7,3 +8,13 @@ print(word_tokenize(example_text))
 
 for i in word_tokenize(example_text):
     print(i)
+
+example_sentence = "This is an example showing off stop word filtration."
+
+stop_words = set(stopwords.words("english"))
+
+words = word_tokenize(example_sentence)
+
+filtered_sentence = [w for w in words if not w in stop_words]
+
+print(filtered_sentence)
